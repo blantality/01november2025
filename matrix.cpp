ba284b003c6396b352f5e93ba9becc8b81666fa8
blantality <parsoffgambit@gmail.com>
2025-11-01 14:04:16 +0300

    sooo kool

--- matrix.cpp
+++ matrix.cpp
@@ -1,48 +1,82 @@
 #include <iostream>
 
-void destroy(int ** mtx, size_t created) 
-{
-  for (size_t i = 0; i < created; i++) 
-  {
+void destroy(int **mtx, size_t created);
+int **create(size_t rows, size_t cols);
+void construct(int **mtx, int init, size_t rows, size_t cols);
+void input(int **mtx, size_t rows, size_t cols);
+void output(int **mtx, size_t rows, size_t cols);
+
+int main() {
+  size_t rows = 0, cols = 0;
+  std::cin >> rows >> cols;
+  if (!std::cin) {
+    std::cerr << "Bad input!\n";
+    return 2;
+  }
+  int **matrix = nullptr;
+  matrix = create(rows, cols);
+  try {
+    matrix = create(rows, cols);
+  } catch (const std::bad_alloc &e) {
+    destroy(matrix, rows);
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
+  std::cout << "Created!\n";
+  construct(matrix, 0, rows, cols);
+  input(matrix, rows, cols);
+  if (!std::cin) {
+    std::cerr << "Bad input!\n";
+    destroy(matrix, rows);
+    return 2;
+  }
+  output(matrix, rows, cols);
+  destroy(matrix, rows);
+}
+
+void destroy(int **mtx, size_t created) {
+  for (size_t i = 0; i < created; ++i) {
     delete[] mtx[i];
   }
   delete[] mtx;
-} 
+}
 
-int **create(size_t rows, size_t cols) 
-{
-  int ** mtx = new int * [rows];
+int **create(size_t rows, size_t cols) {
+  int **mtx = new int *[rows];
   size_t created = 0;
-  try 
-  {  
-    for (size_t i = 0; i < rows; ++i) 
-    {
-      mtx [i] = new int[cols];
-      created++;
+  try {
+    for (size_t i = 0; i < rows; ++i) {
+      mtx[i] = new int[cols];
+      ++created;
     }
-  }
-  catch (const std::bad_alloc &e) 
-  {
+  } catch (const std::bad_alloc &e) {
     destroy(mtx, created);
     throw;
   }
-
   return mtx;
 }
 
-
-int main() {
-  int ** matrix = nullptr;
-  try
-  {
-  matrix = create(5, 5);
+void construct(int **mtx, int init, size_t rows, size_t cols) {
+  for (size_t i = 0; i < rows; ++i) {
+    for (size_t j = 0; j < cols; ++j) {
+      mtx[i][j] = init;
+    }
   }
-  catch (std::bad_alloc & e)
-  {
-    std::cerr << e.what() << "\n";
+}
 
-    return 1;
+void input(int **mtx, size_t rows, size_t cols) {
+  for (size_t i = 0; i < rows; ++i) {
+    for (size_t j = 0; j < cols; ++j) {
+      std::cin >> mtx[i][j];
+    }
   }
+}
 
-  destroy(matrix, 5);
+void output(int **mtx, size_t rows, size_t cols) {
+  for (size_t i = 0; i < rows; ++i) {
+    for (size_t j = 0; j < cols - 1; ++j) {
+      std::cout << mtx[i][j] << ' ';
+    }
+    std::cout << mtx[i][cols - 1] << '\n';
+  }
 }
